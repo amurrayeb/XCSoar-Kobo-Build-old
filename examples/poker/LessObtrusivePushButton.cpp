8cbc2caa03c1e92103218ce002b9c7da2f734a47
Melissa Jenkins <melissa@ubuntu.(none)>
2013-05-13 06:20:01 -0700

    Remove unused examples

--- examples/poker/LessObtrusivePushButton.cpp
+++
@@ -1,5 +0,0 @@
-#include "LessObtrusivePushButton.h"
-
-LessObtrusivePushButton::LessObtrusivePushButton(QWidget *parent) : QPushButton(parent) {}
-
-void LessObtrusivePushButton::keyPressEvent(QKeyEvent *e) {e->ignore();}
