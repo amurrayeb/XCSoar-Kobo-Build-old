418e686c4b50b4ecde0de233803041a1e46711d4
Melissa Jenkins <melissa@ubuntu.(none)>
2013-05-29 15:03:05 -0700

    patch to fix bugs in SDL_gfx and improve performance on line drawing

--- build/scripts/SDL_gfx.sh
+++ build/scripts/SDL_gfx.sh
@@ -4,6 +4,7 @@ ARCHIVE=SDL_gfx-2.0.23.tar.gz
 ARCHIVEDIR=SDL_gfx-2.0.23
 . $KOBO_SCRIPT_DIR/build-common.sh
 
+patch -p0 < $PATCHESDIR/SDL-gfx-kobo.patch
 
 pushd $ARCHIVEDIR
         PREFIX=/${DEVICEROOT} CHOST=${CROSSTARGET} CROSS_PREFIX=${CROSSTARGET} SDL_CONFIG=/${DEVICEROOT}/bin/sdl-config ./configure  --prefix=/${DEVICEROOT}  --host=${CROSSTARGET} --disable-sdltest --disable-mmx --with-sdl-prefix=/${DEVICEROOT}
