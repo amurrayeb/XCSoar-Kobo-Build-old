9d2071014597b6508df69591b8e8f851c9deed76
John Wharington <jwharington@gmail.com>
2013-06-04 22:01:12 +1000

    From 779022bfdc6c203e8a423ece480e8799a679d4d6 Mon Sep 17 00:00:00 2001 Subject: [PATCH 9/9] New SDL patch to support blinking the display
    This uses low-level EPD control to wait for display update to complete,
    this is required as otherwise drawing black then white produces tearing.
    It's a bit ugly really with use of direct call from the generic driver to the fb driver.
    
    
    The patch also ensures the update scheme is set to UPDATE_SCHEME_QUEUE
    and forces monochrome mode.

--- build/scripts/SDL.sh
+++ build/scripts/SDL.sh
@@ -5,7 +5,7 @@ ARCHIVEDIR=SDL-1.2.15
 . $KOBO_SCRIPT_DIR/build-common.sh
 
 # add the hacky patch
-patch -p0 < $PATCHESDIR/SDL-1.2.15-kobo.patch
+patch -p0 < $PATCHESDIR/SDL-1.2.15-kobo-new.patch
 
 pushd $ARCHIVEDIR
         CFLAGS="-DKOBO" ./configure  --prefix=/${DEVICEROOT} --host=${CROSSTARGET} --disable-video-x11 --disable-cdrom --disable-audio --disable-opengl --disable-json
